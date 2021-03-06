b242088b009ff421829de496432e44c84fb7135e
ramonarmen96 <ramonarmen96@gmail.com>
2021-03-06 11:55:25 -0700

    Find film by ID search established

--- WebContent/WEB-INF/views/index.jsp
+++ WebContent/WEB-INF/views/index.jsp
@@ -10,5 +10,22 @@
 <body>
 <h1>My Film Site</h1>
 
+<!-- Find by Last Name -->
+<div>
+	<h3>Search by Film ID:</h3>
+	<form action="findFilmById.do" method="get">
+	<label for="filmId">Film ID:</label> <input type="text" name="id">
+	<input type="submit" value="Search">
+	</form>
+</div>
+<!-- Find by Party -->
+<div>
+	<h3>Search by party:</h3>
+	<form action="presidentInfo" method="get">
+	<label for="party">Party:</label> <input type="text" name="party">
+	<input type="submit" value="Search">
+	</form>
+</div>
+
 </body>
 </html>
--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -3,8 +3,12 @@ package com.skilldistillery.film.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.film.dao.FilmDAO;
+import com.skilldistillery.film.entities.Film;
 
 @Controller
 public class FilmController {
@@ -16,4 +20,36 @@ public class FilmController {
 	public String home() {
 		return "WEB-INF/views/index.jsp";
 	}
+	@RequestMapping(path= {"findFilmById.do"}, method= RequestMethod.GET)
+	public ModelAndView findFilmById(@RequestParam("id")String id) {
+		ModelAndView mv = new ModelAndView();	
+		Integer integerId = new Integer(id);
+		Film film = filmdao.findFilmById(integerId);
+		if(film==null) {
+			mv.setViewName("WEB-INF/views/NoResults.jsp");
+			return mv;		}
+		mv.addObject("film", film);
+		 mv.setViewName("WEB-INF/views/results.jsp");
+		return mv;
+	}
+//	@RequestMapping("GetNumbers.do")
+//	  public ModelAndView getNumbers(@RequestParam(value="howmany", defaultValue="6") int count, HttpSession session) {
+//	    hopper.reset();
+//	    List<String> nums = new ArrayList<>();
+//	    for (int i = 0; i < count; i++) {
+//	      nums.add(hopper.drawBall().getValue());
+//	    }
+//	    ModelAndView mv = new ModelAndView();
+//	    mv.addObject("listOfNumbers", nums);
+//	    // Check in session for history - session.getAttribute(key)
+//	    List< List<String> > history = (List< List<String> >) session.getAttribute("lottoHistory");
+//	    if(history == null) {
+//	      history = new ArrayList<>();
+//	      // Put history in session - session.setAttribute(key, value)
+//	      session.setAttribute("lottoHistory", history);
+//	    }
+//	    history.add(nums);
+//	    mv.setViewName("WEB-INF/form.jsp");
+//	    return mv;
+//	  }
 }
