63d3343bf455e5c320fe0b4f6d8d6700f4926351
Sofia <sgkartist@gmail.com>
2021-03-06 08:30:34 -0700

    Entities and base DAO implementations.

--- src/main/java/com/skilldistillery/film/dao/FilmDAOJdbcImpl.java
+++ src/main/java/com/skilldistillery/film/dao/FilmDAOJdbcImpl.java
@@ -1,8 +1,388 @@
 package com.skilldistillery.film.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
+
+import com.skilldistillery.film.entities.Actor;
+import com.skilldistillery.film.entities.Film;
+import com.skilldistillery.film.entities.Rating;
+
 
 @Component
 public class FilmDAOJdbcImpl implements FilmDAO {
+
+	private static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false";
+	private static String user = "student";
+	private static String pass = "student";
+	private static Connection conn = null;
+
+	public FilmDAOJdbcImpl() {
+		
+	}
+
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(URL, user, pass);
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public Film findFilmById(int filmId) {
+
+		if (filmId < 1)
+			return null;
+		try {
+			String sql = "SELECT * FROM film join language on film.language_id = language.id where film.id = ?;";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, filmId);
+
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+
+				return new Film(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5),
+						rs.getInt(6), rs.getDouble(7), rs.getInt(8), rs.getDouble(9), Rating.valueOf(rs.getString(10)),
+						rs.getString(11), findActorsByFilmId(filmId), rs.getString("language.name"));
+			}
+
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+
+		return null;
+	}
+
+	@Override
+	public Actor findActorById(int actorId) {
+
+		if (actorId < 1)
+			return null;
+
+		try {
+			String sql = "SELECT * FROM actor where actor.id = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, actorId);
+
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+
+				return new Actor(rs.getInt(1), rs.getString(2), rs.getString(3));
+			}
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+
+		return null;
+	}
+
+	@Override
+	public List<Actor> findActorsByFilmId(int filmId) {
+
+		if (filmId < 1)
+			return null;
+
+		try {
+			String sql = "select * from film join film_actor on film.id = film_actor.film_id join actor on film_actor.actor_id = actor.id where film.id = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, filmId);
+
+			ResultSet rs = ps.executeQuery();
+
+			List<Actor> cast = new LinkedList<>();
+			while (rs.next()) {
+
+				cast.add(new Actor(rs.getInt("actor.id"), rs.getString("actor.first_name"),
+						rs.getString("actor.last_name")));
+			}
+
+			return cast;
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+
+		return null;
+	}
+
+	public List<Film> findFilmByKeyword(String keyword) {
+
+		List<Film> filmsMatchedKeyword = new LinkedList<>();
+
+		try {
+			String sql = "select * from film join language on film.language_id = language.id where film.title like ? or film.description like ?;";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, "%" + keyword + "%");
+			ps.setString(2, "%" + keyword + "%");
+
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				filmsMatchedKeyword.add(new Film(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4),
+						rs.getInt(5), rs.getInt(6), rs.getDouble(7), rs.getInt(8), rs.getDouble(9),
+						Rating.valueOf(rs.getString(10)), rs.getString(11), findActorsByFilmId(rs.getInt(1)),
+						rs.getString("language.name")));
+			}
+
+			return filmsMatchedKeyword;
+
+		} catch (SQLException e) {
+			System.out.println(e + "from findFilmByKeyword!!!!!!!!!!!!!!!!!!!!!");
+
+		}
+
+		return null;
+	}
+
+	public Actor createActor(Actor actor) {
+
+		try (Connection conn = DriverManager.getConnection(URL, user, pass)) {
+
+			conn.setAutoCommit(false); // START TRANSACTION
+			String sql = "INSERT INTO actor (first_name, last_name) " + " VALUES (?,?)";
+			PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			stmt.setString(1, actor.getFirstName());
+			stmt.setString(2, actor.getLastName());
+			int updateCount = stmt.executeUpdate();
+			if (updateCount == 1) {
+				ResultSet keys = stmt.getGeneratedKeys();
+				if (keys.next()) {
+					int newActorId = keys.getInt(1);
+					actor.setId(newActorId);
+					if (actor.getFilms() != null && actor.getFilms().size() > 0) {
+						sql = "INSERT INTO film_actor (film_id, actor_id) VALUES (?,?)";
+						stmt = conn.prepareStatement(sql);
+						for (Film film : actor.getFilms()) {
+							stmt.setInt(1, film.getId());
+							stmt.setInt(2, newActorId);
+							updateCount = stmt.executeUpdate();
+						}
+					}
+				}
+			} else {
+				actor = null;
+			}
+			conn.commit(); // COMMIT TRANSACTION
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+			if (conn != null) {
+				try {
+					conn.rollback();
+				} catch (SQLException sqle2) {
+					System.err.println("Error trying to rollback");
+				}
+			}
+			throw new RuntimeException("Error inserting actor " + actor);
+		}
+		return actor;
+	}
+
+	public boolean saveActor(Actor actor) {
+
+		try (Connection conn = DriverManager.getConnection(URL, user, pass)) {
+
+			conn.setAutoCommit(false); // START TRANSACTION
+			String sql = "UPDATE actor SET first_name=?, last_name=? " + " WHERE id=?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, actor.getFirstName());
+			stmt.setString(2, actor.getLastName());
+			stmt.setInt(3, actor.getId());
+			int updateCount = stmt.executeUpdate();
+			if (updateCount == 1) {
+				// Replace actor's film list
+				sql = "DELETE FROM film_actor WHERE actor_id = ?";
+				stmt = conn.prepareStatement(sql);
+				stmt.setInt(1, actor.getId());
+				updateCount = stmt.executeUpdate();
+				sql = "INSERT INTO film_actor (film_id, actor_id) VALUES (?,?)";
+				stmt = conn.prepareStatement(sql);
+				for (Film film : actor.getFilms()) {
+					stmt.setInt(1, film.getId());
+					stmt.setInt(2, actor.getId());
+					updateCount = stmt.executeUpdate();
+				}
+				conn.commit(); // COMMIT TRANSACTION
+			}
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+			if (conn != null) {
+				try {
+					conn.rollback();
+				} // ROLLBACK TRANSACTION ON ERROR
+				catch (SQLException sqle2) {
+					System.err.println("Error trying to rollback");
+				}
+			}
+			return false;
+		}
+		return true;
+	}
+
+	public boolean deleteActor(Actor actor) {
+
+		try (Connection conn = DriverManager.getConnection(URL, user, pass)) {
+
+			conn.setAutoCommit(false); // START TRANSACTION
+			String sql = "DELETE FROM film_actor WHERE actor_id = ?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, actor.getId());
+			int updateCount = stmt.executeUpdate();
+			sql = "DELETE FROM actor WHERE id = ?";
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, actor.getId());
+			updateCount = stmt.executeUpdate();
+			conn.commit(); // COMMIT TRANSACTION
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+			if (conn != null) {
+				try {
+					conn.rollback();
+				} catch (SQLException sqle2) {
+					System.err.println("Error trying to rollback");
+				}
+			}
+			return false;
+		}
+		return true;
+	}
+	
+	public Film createFilm(Film film) {
+
+		try (Connection conn = DriverManager.getConnection(URL, user, pass)) {
+
+			conn.setAutoCommit(false); // START TRANSACTION
+			
+			String sql = "insert into film(title, language_id) values (?, 1)";
+			PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, film.getTitle());
+			
+			int updateCount = ps.executeUpdate();
+			if (updateCount == 1) {
+				ResultSet keys = ps.getGeneratedKeys();
+				if (keys.next()) {
+					int newActorId = keys.getInt(1);
+					film.setId(newActorId);
+					if (film.getCast() != null && film.getCast().size() > 0) {
+						sql = "INSERT INTO film_actor (film_id, actor_id) VALUES (?,?)";
+						ps = conn.prepareStatement(sql);
+						
+						ps.setInt(1, film.getId());
+						
+						for (Actor actor : film.getCast()) {
+
+							ps.setInt(2, actor.getId());
+							updateCount = ps.executeUpdate();
+						}
+					}
+				}
+			} else {
+				film = null;
+			}
+			conn.commit(); // COMMIT TRANSACTION
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+			if (conn != null) {
+				try {
+					conn.rollback();
+				} catch (SQLException sqle2) {
+					System.err.println("Error trying to rollback");
+				}
+			}
+			throw new RuntimeException("Error inserting film " + film);
+		}
+		return film;
+	}
+	
+	public boolean saveFilm(Film film) {
+
+		try (Connection conn = DriverManager.getConnection(URL, user, pass)) {
+
+			conn.setAutoCommit(false); // START TRANSACTION
+			
+			String sql = "UPDATE film SET title=?, rating=? , description=? " + " WHERE id=?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			
+			stmt.setString(1, film.getTitle());
+			stmt.setString(2, film.getRating());
+			stmt.setString(3, film.getDescription());
+			stmt.setInt(4, film.getId());
+			
+			int updateCount = stmt.executeUpdate();
+			if (updateCount == 1) {
+				// Replace actor's film list
+				sql = "DELETE FROM film_actor WHERE film_id = ?";
+				stmt = conn.prepareStatement(sql);
+				stmt.setInt(1, film.getId());
+				updateCount = stmt.executeUpdate();
+				sql = "INSERT INTO film_actor (film_id, actor_id) VALUES (?,?)";
+				stmt = conn.prepareStatement(sql);
+				stmt.setInt(1, film.getId());
+				
+				for (Actor actor : film.getCast()) {
+					
+					stmt.setInt(2, actor.getId());
+					updateCount = stmt.executeUpdate();
+				}
+				conn.commit(); // COMMIT TRANSACTION
+			}
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+			if (conn != null) {
+				try {
+					conn.rollback();
+				} // ROLLBACK TRANSACTION ON ERROR
+				catch (SQLException sqle2) {
+					System.err.println("Error trying to rollback");
+				}
+			}
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean deleteFilm(Film film) {
+
+		try (Connection conn = DriverManager.getConnection(URL, user, pass)) {
+
+			conn.setAutoCommit(false); // START TRANSACTION
+			
+			String sql = "DELETE FROM film_actor WHERE film_id = ?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, film.getId());
+			
+			int updateCount = stmt.executeUpdate();
+			
+			sql = "DELETE FROM film WHERE id = ?";
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, film.getId());
+			
+			updateCount = stmt.executeUpdate();
+			
+			conn.commit(); // COMMIT TRANSACTION
+		
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+			if (conn != null) {
+				try {
+					conn.rollback();
+				} catch (SQLException sqle2) {
+					System.err.println("Error trying to rollback");
+				}
+			}
+			return false;
+		}
+		return true;
+	}
 
 }
--- src/main/java/com/skilldistillery/film/entities/Film.java
+++ src/main/java/com/skilldistillery/film/entities/Film.java
@@ -1,5 +1,200 @@
 package com.skilldistillery.film.entities;
 
-public class Film {
+import java.util.List;
+import java.util.Objects;
 
+public class Film {
+	
+	private int id;
+	private String title;
+	private String description;
+	private String releaseYear;
+	
+	private int languageId;
+	private int rentalDuration;
+	private double rentalRate;
+	private Integer length;
+	private double replacementCost;
+	private Rating rating;
+	private String specialFeatures;
+	private String language;
+	
+	private List<Actor> cast;
+	
+	public Film() {}
+	
+	public Film(String title) {
+		super();
+		this.title = title;
+	}
+
+	public Film(int id, String title, String description, String releaseYear, int languageId, int rentalDuration,
+			double rentalRate, Integer length, double replacementCost, Rating rating, String specialFeatures, List<Actor> cast, String language) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.releaseYear = releaseYear;
+		this.languageId = languageId;
+		this.rentalDuration = rentalDuration;
+		this.rentalRate = rentalRate;
+		this.length = length;
+		this.replacementCost = replacementCost;
+		this.rating = rating;
+		this.specialFeatures = specialFeatures;
+		this.cast = cast;
+		this.language = language;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Film [id=");
+		builder.append(id);
+		builder.append(", title=");
+		builder.append(title);
+		builder.append(", description=");
+		builder.append(description);
+		builder.append(", releaseYear=");
+		builder.append(releaseYear);
+		builder.append(", languageId=");
+		builder.append(languageId);
+		builder.append(", rentalDuration=");
+		builder.append(rentalDuration);
+		builder.append(", rentalRate=");
+		builder.append(rentalRate);
+		builder.append(", length=");
+		builder.append(length);
+		builder.append(", replacementCost=");
+		builder.append(replacementCost);
+		builder.append(", rating=");
+		builder.append(rating);
+		builder.append(", specialFeatures=");
+		builder.append(specialFeatures);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Film other = (Film) obj;
+		return id == other.id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(String releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+	public int getLanguageId() {
+		return languageId;
+	}
+
+	public void setLanguageId(int languageId) {
+		this.languageId = languageId;
+	}
+
+	public int getRentalDuration() {
+		return rentalDuration;
+	}
+
+	public void setRentalDuration(int rentalDuration) {
+		this.rentalDuration = rentalDuration;
+	}
+
+	public double getRentalRate() {
+		return rentalRate;
+	}
+
+	public void setRentalRate(double rentalRate) {
+		this.rentalRate = rentalRate;
+	}
+
+	public Integer getLength() {
+		return length;
+	}
+
+	public void setLength(Integer length) {
+		this.length = length;
+	}
+
+	public double getReplacementCost() {
+		return replacementCost;
+	}
+
+	public void setReplacementCost(double replacementCost) {
+		this.replacementCost = replacementCost;
+	}
+
+	public String getRating() {
+		return "" + rating;
+	}
+
+	public void setRating(Rating rating) {
+		this.rating = rating;
+	}
+
+	public String getSpecialFeatures() {
+		return specialFeatures;
+	}
+
+	public void setSpecialFeatures(String specialFeatures) {
+		this.specialFeatures = specialFeatures;
+	}
+
+	public List<Actor> getCast() {
+		return cast;
+	}
+
+	public void setCast(List<Actor> cast) {
+		this.cast = cast;
+	}
+	
+	
 }
